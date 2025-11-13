1e05cce2965e6654f5e802ffeea198ef6281e10d
Min Kyaw <mintheinkyawprd@gmail.com>
2025-11-13 16:08:29 +0000

    Add Temperature class and update Main to demonstrate its usage

--- src/ie/atu/encapsulation/Main.java
+++ src/ie/atu/encapsulation/Main.java
@@ -11,13 +11,20 @@ public class Main {
         student.studentId = -1;  // Invalid ID
         student.gpa = 5.5;        // GPA above 4.0
         
+        System.out.println();
         System.out.println("Student: " + student.name);
         System.out.println("Student ID: " + student.studentId);
         System.out.println("GPA: " + student.gpa);
 
+        System.out.println();
         // Create a SecretMessage object
         SecretMessage secret = new SecretMessage("This is a secret!");
         // Type secret. and observe autocomplete - message field is not visible
         secret.displayMessage();
+
+        System.out.println();
+        Temperature temp = new Temperature (25.0);
+        temp.setCelsius(25.0);
+        System.out.println("Temperature: " + temp.getCelsius() + "°C");
     }
 }
---
+++ src/ie/atu/encapsulation/Temperature.java
@@ -0,0 +1,16 @@
+package ie.atu.encapsulation;
+
+public class Temperature {
+    private double celsius;
+
+    public Temperature(double celsius) {
+        this.celsius = celsius;
+    }
+    public double getCelsius() {
+        return celsius;
+    }
+    public void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
+    
+}
